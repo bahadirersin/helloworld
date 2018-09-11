495789da8035a5221ca8ecfc724ee498764d7b0b
Bahadır Ersin <bahadir@bahadirs-mac.local>
2018-09-11 18:03:32 +0300

    Initial Commit

---
+++ halloworld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  halloworld
+//
+//  Created by bahadir on 11.09.2018.
+//  Copyright © 2018 bahadir. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
